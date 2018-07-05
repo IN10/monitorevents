6d454ea87307540c97c9b83314525d311038b4e5
Jakob Buis <jakob@jakobbuis.nl>
2018-07-05 23:44:02 +0200

    websites have state

--- app/Website.php
+++ app/Website.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Website extends Model
 {
-    protected $fillable = ['name', 'url'];
+    protected $fillable = ['name', 'url', 'state'];
+    protected $casts = [
+        'state' => 'array',
+    ];
 }
